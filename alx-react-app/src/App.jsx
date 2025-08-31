7bbf573f3cf1807f7fe12cfa24bf5bc0466dec29
Chikwado stanley Elochukwu <chikwadostanley0@gmail.com>
2025-08-31 15:10:05 -0700

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,9 @@
 // src/App.jsx
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
